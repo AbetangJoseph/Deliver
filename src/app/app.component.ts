529fb1f2962014602cea4f2481b0184014842d2a
Abetang Joseph <josephabetang@gmail.com>
2019-10-04 13:38:06 +0100

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Deliver';
+  navData: object = {
+    mainNav: [
+      'Courier Services',
+      'Food Delivery',
+      'How It Works',
+      'FAQs',
+      'Sign In',
+      'Become A Dispatcher'
+    ],
+    subNav: ['Home', 'Payment Methods', 'My Orders', 'Cart']
+  };
+  title: string = 'Deliver';
 }
---
+++ src/app/components/Nav/Nav.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './Nav.component.html',
+  styleUrls: ['./Nav.component.css']
+})
+export class NavComponent implements OnInit {
+  @Input() data: object;
+  constructor() {}
+
+  ngOnInit() {}
+}
